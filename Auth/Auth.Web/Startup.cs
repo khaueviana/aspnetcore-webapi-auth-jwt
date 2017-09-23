dcc643081e695585205fc088555405348a438a68
Khauê Viana <khaueviana@gmail.com>
2017-09-23 16:49:09 -0300

    Setup Project

---
+++ Auth/Auth.Web/Startup.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Auth.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Threading.Tasks;
+
+namespace Auth.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var config = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json")
+              .Build();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(cfg =>
+            {
+                cfg.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = ctx =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == 200)
+                        {
+                            ctx.Response.StatusCode = 401;
+                            return Task.FromResult<object>(null);
+                        }
+
+                        ctx.Response.Redirect(ctx.RedirectUri);
+                        return Task.FromResult<object>(null);
+                    }
+                };
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("CanChicoFeelingsRead", policy => policy.RequireClaim("ChicoFeelings", "Get"));
+                options.AddPolicy("CanChicoFeelingsCreate", policy => policy.RequireClaim("ChicoFeelings", "Create"));
+                options.AddPolicy("CanChicoFeelingsSudoCreate", policy => policy.RequireClaim("ChicoFeelings", "SudoCreate"));
+            });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
